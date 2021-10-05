d8f6ef1349265308d380b105fe85aa29d5c5f84e
Nadiah <042574432@ecampus.ut.ac.id>
2021-10-05 23:23:54 +0700

    add ui landingpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
+
+import Home from './pages/Landingpage/Home';
+import Login from './pages/Landingpage/Login';
+import SignUp from "./pages/Landingpage/SignUp";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path={["/", "home"]} component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
